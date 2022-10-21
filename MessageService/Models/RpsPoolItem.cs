1475a6936bc90c12943ad436004bc5b9c33e2ced
Gitea <gitea@fake.local>
2022-10-21 15:52:36 +0500

    удаление не нужного

--- MessageService/Models/RpsPoolItem.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MessageService.Models {
-
-    internal class RpsPoolItem {
-        public RpsPoolItem Next { get; set; }
-        public long StartingMilliseconds { get; set; } = -1000;
-    }
-}
