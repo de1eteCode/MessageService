e8630aa952d6e1ea1dc36ec1530f294cd510a6ea
de1eteCode <skozlov2015@yandex.ru>
2022-11-12 15:58:44 +0500

    Добавление базовых сущностей системы

---
+++ Domain/EntityModels/ChatGroup.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.Models;
+
+/// <summary>
+/// Объединение сущностей <see cref="Chat"/> и <see cref="Group"/>
+/// </summary>
+public class ChatGroup : BaseEntity {
+
+    /// <summary>
+    /// Идентификатор сущности <see cref="Chat"/>
+    /// </summary>
+    public Guid ChatUID { get; set; }
+
+    /// <summary>
+    /// Идентификатор сущности <see cref="Group"/>
+    /// </summary>
+    public Guid GroupUID { get; set; }
+
+    /// <summary>
+    /// Является данная связь расторгнутой
+    /// </summary>
+    public bool IsDeleted { get; set; }
+
+    /// <summary>
+    /// Чат Telegram
+    /// </summary>
+    public virtual Chat Chat { get; set; } = null!;
+
+    /// <summary>
+    /// Группа
+    /// </summary>
+    public virtual Group Group { get; set; } = null!;
+}
---
+++ Domain/EntityModels/User.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Models;
+
+/// <summary>
+/// Пользователь в системе
+/// </summary>
+public class User : BaseEntity {
+
+    public User() {
+        UserGroups = new HashSet<UserGroup>();
+    }
+
+    /// <summary>
+    /// Идентификатор Telegram
+    /// </summary>
+    public long TelegramId { get; set; }
+
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Идентификатор <see cref="Role"/>
+    /// </summary>
+    public Guid RoleUID { get; set; }
+
+    /// <summary>
+    /// Роль пользователя
+    /// </summary>
+    public virtual Role Role { get; set; } = null!;
+
+    /// <summary>
+    /// Группы, в которые включен пользователь
+    /// </summary>
+    public virtual ICollection<UserGroup> UserGroups { get; set; }
+}
