d8f99a975ea3c34dcdc428758e2a27d7e9fcf5b5
de1eteCode <skozlov2015@yandex.ru>
2022-11-14 21:57:29 +0500

    Добавление странички групп

---
+++ WebUI/Pages/Groups/Groups.razor.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebUI.Pages.Groups;
+
+public class GroupsBase : ComponentBase {
+
+    [Inject]
+    public IDataContext Context { get; set; } = default!;
+
+    public IEnumerable<Group> Groups { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync() {
+        Groups = await Context.Groups.ToListAsync();
+    }
+}
