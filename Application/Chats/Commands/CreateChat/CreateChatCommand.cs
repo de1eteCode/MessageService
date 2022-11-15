a492d841e20fe8d5e935cad9c983f8d4d3f2ada7
Gitea <gitea@fake.local>
2022-11-15 09:29:15 +0500

    Загатовка для MediatR

---
+++ Application/Chats/Commands/CreateChat/CreateChatCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Chats.Commands.CreateChat;
+public record CreateChatCommand : IRequest<Chat> {
+    public string Name { get; set; } = default!;
+    public bool IsJoined { get; set; } = default!;
+    public string KickedUserLogin { get; set; } = default!;
+    public long KickedUserId { get; set; } = default!;
+    public DateTime Time { get; set; } = default!;
+}
--- Infrastructure/IHandlerHostedService.cs
+++ Infrastructure/IHandlerHostedService.cs
@@ -3,6 +3,13 @@ using Microsoft.Extensions.Hosting;
 
 namespace Infrastructure;
 
+/// <summary>
+/// Интерфейс сервиса для хостинга
+/// </summary>
 public interface IHandlerHostedService : IHostedService {
+
+    /// <summary>
+    /// Состояние сервиса
+    /// </summary>
     public ServiceState State { get; }
 }
