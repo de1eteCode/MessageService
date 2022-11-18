cb4a2b8ed590901419cccd0040af474acad26e62
Gitea <gitea@fake.local>
2022-11-18 13:59:50 +0500

    Добавление команд групп для медиатора

---
+++ Application/Groups/Commands/CreateGroup/CreateGroupCommand.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Groups.Commands.CreateGroup;
+public record CreateGroupCommand : IRequest<Group> {
+    public string Name { get; set; } = default!;
+    public Guid OwnerUserUID { get; set; }
+}
---
+++ Application/Groups/Commands/CreateGroup/CreateGroupCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Groups.Commands.CreateGroup;
+
+public class CreateGroupCommandValidator : AbstractValidator<CreateGroupCommand> {
+
+    public CreateGroupCommandValidator() {
+        RuleFor(e => e.Name)
+            .MinimumLength(1)
+            .MaximumLength(255)
+            .NotEmpty();
+
+        RuleFor(e => e.OwnerUserUID)
+            .NotEmpty();
+    }
+}
