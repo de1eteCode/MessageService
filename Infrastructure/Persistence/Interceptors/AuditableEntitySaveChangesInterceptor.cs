53ee8bf08087b4b8977bc364012f34e118be7fef
Gitea <gitea@fake.local>
2023-02-15 13:45:40 +0500

    Сервис времени

---
+++ Infrastructure/Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -0,0 +1,53 @@
+﻿using Application.Common.Interfaces;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Infrastructure.Persistence.Interceptors;
+
+internal class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor {
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IDateTime _dateTime;
+
+    public AuditableEntitySaveChangesInterceptor(ICurrentUserService currentUserService, IDateTime dateTime) {
+        _currentUserService = currentUserService;
+        _dateTime = dateTime;
+    }
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result) {
+        UpdateEntities(eventData.Context);
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default) {
+        UpdateEntities(eventData.Context);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext? dbContext) {
+        if (dbContext == null) {
+            return;
+        }
+
+        foreach (var entry in dbContext.ChangeTracker.Entries<BaseAuditableEntity>()) {
+            if (entry.State == EntityState.Added) {
+                entry.Entity.Created = _dateTime.Now;
+                entry.Entity.CreatedBy = _currentUserService.UserId?.ToString("N") ?? null;
+            }
+
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified || HasChangedOwnedEntities(entry)) {
+                entry.Entity.LastModified = _dateTime.Now;
+                entry.Entity.LastModifiedBy = _currentUserService.UserId?.ToString("N") ?? null;
+            }
+        }
+    }
+
+    private static bool HasChangedOwnedEntities(EntityEntry entry) =>
+        entry.References.Any(r =>
+            r.TargetEntry != null &&
+            r.TargetEntry.Metadata.IsOwned() &&
+            (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
+}
